fe4653af159df78727ed7059eced6ea707d7c330
gabriel <franklingabriel158@gmail.com>
2022-04-20 17:56:14 -0300

    impletação de rotas

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Logo.css';
 import logo from '../../assets/imgs/logo.png'
 
 const Logo = (props) =>  
   <aside className='logo' >
-    <a href="/" className="logo">
+    <Link to="/" className="logo">
       <img src={logo} alt="logo" />
-    </a>
+    </Link>
   </aside>
 
 export default Logo;
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,17 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Router';
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
-import Home from '../components/home/Home';
 import Footer from '../components/template/Footer';
 
 const App = props => 
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Home />
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Router />
+      <Footer />
+    </div>
+  </BrowserRouter>
 
 export default App; 
