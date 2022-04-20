9ae354a45455528c016eb07261dc2e16e90c3392
gabriel <franklingabriel158@gmail.com>
2022-04-20 18:46:52 -0300

    implementa formulário de cadastro

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -20,6 +20,7 @@ class UserCrud extends Component {
 
   state = { ...initialState }
 
+
   clear() {
     this.setState({ user: initialState.user })
   }
@@ -30,9 +31,9 @@ class UserCrud extends Component {
     const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
     axios[method](url, user)
       .then(resp => {
-        const list = this.getUpdatedList(resp.data)
-        this.setState({ user: initialState.user, list })
-      })
+        const list = this.getUpdatedList(resp.data);
+        this.setState({ user: initialState.user, list });
+      });
   }
 
   getUpdatedList(user) {
@@ -41,10 +42,68 @@ class UserCrud extends Component {
     return list;
   }
 
+  updateField(event) {
+    const user = { ...this.state.user };
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
+  }
+
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Nome</label>
+              <input
+                type="text" 
+                className="form-control"
+                name="name"
+                value={this.state.user.name}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o nome..."
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+          <label>E-mail</label>
+              <input
+                type="text" 
+                className="form-control"
+                name="email"
+                value={this.state.user.email}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o e-mail..."
+               /> 
+          </div>
+        </div>
+
+        <hr />
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button 
+              className="btn btn-primary ml-2"
+              onClick={e => this.save(e)}
+            >
+              Salvar
+            </button>
+            <button 
+              className="btn btn-secondary ml-2"
+              onClick={e => this.clear(e)}
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+
   render() {
     return (
       <Main {...headerProps}>
-        Cadastro de Usuário
+        {this.renderForm()}
       </Main>
     )
   }
